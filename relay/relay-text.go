e54623630d39e31759b91eebb012465b5041aabe
xqx333 <141810964+xqx333@users.noreply.github.com>
2025-03-27 00:53:04 +0800

    Update relay-text.go

--- relay/relay-text.go
+++ relay/relay-text.go
@@ -88,6 +88,8 @@ func TextHelper(c *gin.Context) (openaiErr *dto.OpenAIErrorWithStatusCode) {
 		c.Set("textRequest", textRequest)
 	}
 
+	common.SysLog(fmt.Sprintf("textRequest: %w", textRequest.Messages))
+
 	// map model name
 	//isModelMapped := false
 	modelMapping := c.GetString("model_mapping")
