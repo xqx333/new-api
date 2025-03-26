ad78d590083abc05d79378e7be0b853c2682a0f1
Roodra Nambisan <gvbwrdfvc@gmail.com>
2025-03-26 21:58:24 +0800

    Update cf_worker.go

--- service/cf_worker.go
+++ service/cf_worker.go
@@ -4,7 +4,6 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
-	"io"
 	"net/http"
 	"one-api/common"
 	"one-api/setting"
@@ -44,8 +43,8 @@ func DoWorkerRequest(req *WorkerRequest) (*http.Response, error) {
 	return http.Post(workerUrl, "application/json", bytes.NewBuffer(workerPayload))
 }
 
+
 func DoDownloadRequest(originUrl string) (resp *http.Response, err error) {
-	maxImageSize := int64(common.MaxImageSize * 1024 * 1024)
 	requestTimeout := common.RequestTimeout
 
 	client := &http.Client{}
@@ -59,45 +58,13 @@ func DoDownloadRequest(originUrl string) (resp *http.Response, err error) {
 			URL: originUrl,
 			Key: setting.WorkerValidKey,
 		}
-		resp, err = DoWorkerRequest(req)
-		if err != nil {
-			return nil, err
-		}
+		return DoWorkerRequest(req)
 	} else {
 		common.SysLog(fmt.Sprintf("downloading from origin: %s", originUrl))
-		req, err := http.NewRequest(http.MethodGet, originUrl, nil)
+		req, err := http.NewRequest("GET", originUrl, nil)
 		if err != nil {
 			return nil, err
 		}
-		resp, err = client.Do(req)
-		if err != nil {
-			return nil, err
-		}
+		return client.Do(req)
 	}
-
-	// 如果未设置最大图片大小，则直接返回响应
-	if maxImageSize <= 0 {
-		return resp, nil
-	}
-	
-	// 使用io.LimitReader限制读取的字节数
-	limitedReader := io.LimitReader(resp.Body, maxImageSize+1) // 读多一个字节以检测是否超出限制
-
-	// 读取数据到缓冲区
-	data, err := io.ReadAll(limitedReader)
-	if err != nil {
-		resp.Body.Close()
-		return nil, fmt.Errorf("error reading response body: %v", err)
-	}
-
-	// 检查是否超过最大图片大小
-	if int64(len(data)) > maxImageSize {
-		resp.Body.Close()
-		return nil, fmt.Errorf("file size exceeds maximum allowed size of %d bytes", maxImageSize)
-	}
-
-	// 将读取的数据重新封装到新的ReadCloser中
-	resp.Body = io.NopCloser(bytes.NewBuffer(data))
-
-	return resp, nil
 }
