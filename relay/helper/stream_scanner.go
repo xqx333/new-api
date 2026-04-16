c7db29d1305db6747d74e707dc9686cf43e89a73
xqx333 <gvbwrdfvc@gmail.com>
2026-04-16 20:24:09 +0800

    Revert "修复上游新增测试失败点"
    This reverts commit 306438a030fedbefc95a52c3dd5f94c10c63bb6f.

--- relay/helper/stream_scanner.go
+++ relay/helper/stream_scanner.go
@@ -40,9 +40,8 @@ func StreamScannerHandler(c *gin.Context, resp *http.Response, info *relaycommon
 		return
 	}
 
-	if info.StreamStatus == nil {
-		info.StreamStatus = relaycommon.NewStreamStatus()
-	}
+	// 无条件新建 StreamStatus
+	info.StreamStatus = relaycommon.NewStreamStatus()
 
 	// 确保响应体总是被关闭
 	defer func() {
